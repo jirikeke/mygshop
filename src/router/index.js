2cfc0d169d2936c2683258b0789d0665d17c21b4
llh <2441450981@qq.com>
2022-08-30 18:26:23 +0800

    init gshop

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Msite from '@/pages/Msite/Msite.vue'
+import Order from '@/pages/Order/Order.vue'
+import Profile from '@/pages/Profile/Profile.vue'
+import Search from '@/pages/Search/Search.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/msite',
+      component: Msite
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/search',
+      component: Search
+    }
+  ]
+})
